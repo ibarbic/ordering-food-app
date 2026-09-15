29bcbeb4775a7c378ccf76f742b3a173354151ad
Ivan <ivanbarbic13@gmail.com>
2026-09-15 16:09:00 +0200

    finished cart, finished adding new address for deliver and select previous addresses

--- frontend/food-ordering/src/component/Cart/Cart.jsx
+++ frontend/food-ordering/src/component/Cart/Cart.jsx
@@ -1,10 +1,55 @@
 /* eslint-disable no-unused-vars */
-import { Divider } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  Divider,
+  Grid,
+  Modal,
+  TextField,
+} from "@mui/material";
 import React from "react";
 import CartItem from "./CartItem";
+import AddressCard from "./AddressCard";
+import AddLocationAltIcon from "@mui/icons-material/AddLocationAlt";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  outline: "none",
+  boxShadow: 24,
+  p: 4,
+};
+
+const initialValues = {
+  streetAddress: "",
+  state: "",
+  zipcode: "",
+  city: "",
+};
+
+const validationScheme = Yup.object().shape({
+  streetAddress: Yup.string().required("Street address is required"),
+  state: Yup.string().required("State is required"),
+  zipcode: Yup.number().required("Zipcode is required"),
+  city: Yup.string().required("City is required"),
+});
 
 const items = [1, 1];
 const Cart = () => {
+  const [open, setOpen] = React.useState(false);
+  const createOrderUsingSelectedAddress = () => {};
+  const handleOpenAddressModal = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleSubmit = (value) => {
+    console.log("form value", value)
+  };
   return (
     <div>
       <main className="lg:flex justify-between">
@@ -12,50 +57,146 @@ const Cart = () => {
           {items.map((item) => (
             <CartItem />
           ))}
-        <Divider />
-        <div className="billlDetails px-5 text-sm">
+          <Divider />
+          <div className="billlDetails px-5 text-sm">
             <p className="font-extralight py-5">Bill Details</p>
             <div className="space-y-3">
-                <div className="flex justify-between text-gray-400">
-                    <p>Item Total</p>
-                    <p>€200</p>
-
-                </div>
-                <div className="flex justify-between text-gray-400">
-                    <p>Delivery Fee</p>
-                    <p>€5.99</p>
-
-                </div>
-                <div className="flex justify-between text-gray-400">
-                    <p>GST and Restaurant Charges</p>
-                    <p>€21</p>
-
-                </div>
-                <Divider/>
-
+              <div className="flex justify-between text-gray-400">
+                <p>Item Total</p>
+                <p>€200</p>
+              </div>
+              <div className="flex justify-between text-gray-400">
+                <p>Delivery Fee</p>
+                <p>€5.99</p>
+              </div>
+              <div className="flex justify-between text-gray-400">
+                <p>GST and Restaurant Charges</p>
+                <p>€21</p>
+              </div>
+              <Divider />
             </div>
             <div className="flex justify-between text-gray-400">
-                <p>Total pay</p>
-                <p>€227</p>
-
+              <p>Total pay</p>
+              <p>€227</p>
             </div>
-            
-        </div>
+          </div>
         </section>
-        <Divider orientation="vertical" flexItem/>
+        <Divider orientation="vertical" flexItem />
         <section className="lg:w-[70%] flex justify-center px-5 pb-10 lg:pb-0">
-            <div>
-                <h1 className="text-center font-semibold text-2xl py-10">
-                    Choose Delivery Address
-                </h1>
-                <div className="flex gap-5 flex-wrap justify-center">
+          <div>
+            <h1 className="text-center font-semibold text-2xl py-10">
+              Choose Delivery Address
+            </h1>
+            <div className="flex gap-5 flex-wrap justify-center">
+              {[1, 1, 1, 1, 1].map((item) => (
+                <AddressCard
+                  handleSelectAddress={createOrderUsingSelectedAddress}
+                  item={item}
+                  showButton={true}
+                />
+              ))}
 
+              <Card className="flex gap-5 w-64 p-5">
+                <AddLocationAltIcon />
+                <div className="space-y-3 text-gray-500">
+                  <h1 className="font-semibold text-lg text-white">
+                    Add New Address
+                  </h1>
+
+                  <Button
+                    variant="outlined"
+                    fullWidth
+                    onClick={handleOpenAddressModal}
+                  >
+                    Add
+                  </Button>
                 </div>
+              </Card>
             </div>
-
+          </div>
         </section>
-        
       </main>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationScheme}
+            onSubmit={handleSubmit}
+          >
+          {({ errors, touched }) => (
+            <Form>
+  <Grid container spacing={2}>
+    <Grid size = {12}>
+      <Field
+        as={TextField}
+        name="streetAddress"
+        label="Street Address"
+        fullWidth
+        variant="outlined"
+        error={touched.streetAddress && Boolean(errors.streetAddress)}
+        helperText={
+          <ErrorMessage name="streetAddress" render={(msg) => <span className="text-red-600">{msg}</span>} />
+        }
+      />
+    </Grid>
+
+    {/* State + Zipcode share one row */}
+    <Grid size={6}>
+      <Field
+        as={TextField}
+        name="state"
+        label="State"
+        fullWidth
+        variant="outlined"
+        error={touched.state && Boolean(errors.state)}
+        helperText={
+          <ErrorMessage name="state" render={(msg) => <span className="text-red-600">{msg}</span>} />
+        }
+      />
+    </Grid>
+    <Grid size={6}>
+      <Field
+        as={TextField}
+        name="zipcode"
+        label="Zipcode"
+        fullWidth
+        variant="outlined"
+        error={touched.zipcode && Boolean(errors.zipcode)}
+        helperText={
+          <ErrorMessage name="zipcode" render={(msg) => <span className="text-red-600">{msg}</span>} />
+        }
+      />
+    </Grid>
+
+    <Grid size={12}>
+      <Field
+        as={TextField}
+        name="city"
+        label="City"
+        fullWidth
+        variant="outlined"
+        error={touched.city && Boolean(errors.city)}
+        helperText={
+          <ErrorMessage name="city" render={(msg) => <span className="text-red-600">{msg}</span>} />
+        }
+      />
+    </Grid>
+
+    <Grid item xs={12}>
+      <Button type="submit" variant="contained" color="primary" fullWidth>
+        Save Address
+      </Button>
+    </Grid>
+  </Grid>
+</Form>)}
+          </Formik>
+        </Box>
+      </Modal>
     </div>
   );
 };
